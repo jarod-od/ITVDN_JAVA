9d63bd9f5d100a74d615a23412d1636315e250a0
Jarod <konstantin.zalevsky@gmail.com>
2019-06-16 00:30:19 +0300

    meet with arrays

--- src/com/jarod/itvdn/main/ArraysV2.java
+++ src/com/jarod/itvdn/main/ArraysV2.java
@@ -4,5 +4,17 @@ public class ArraysV2 {
     public static void main(String[] args) {
         int[] arrs = new int[5];
 
+//        Заполнение массива
+        for (int i = 0; i < arrs.length; i++){
+            arrs [i] = i *2;
+            System.out.println(arrs [i]);
+        }
+
+        // Специальная запись цикла for для прохождения элементов массива (Java 8)
+        // Аналогична конструкции foreach, используемой в других языках программирования (например C#, PHP)
+        // for (int ключ, массив)
+        for (int anArray : arrs){
+            System.out.print(anArray +" ");
+        }
     }
 }

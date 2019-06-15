e27b050a740c33fdc8f62d104fdd192bab77a143
Jarod <konstantin.zalevsky@gmail.com>
2019-06-16 00:03:10 +0300

    meet with arrays

---
+++ src/com/jarod/itvdn/main/Arrays.java
@@ -0,0 +1,12 @@
+package com.jarod.itvdn.main;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        numbers[3] = 55;
+        numbers[0] = 75;
+        System.out.print(numbers[3]);
+        System.out.print("\t" + numbers[1] + "\t" + numbers[0]);
+}
+}
---
+++ src/com/jarod/itvdn/main/ArraysV2.java
@@ -0,0 +1,8 @@
+package com.jarod.itvdn.main;
+
+public class ArraysV2 {
+    public static void main(String[] args) {
+        int[] arrs = new int[5];
+
+    }
+}
